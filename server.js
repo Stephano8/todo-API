aca588f666ea582afe84d4ddb55bfe730b139eb4
Stephano Lioe Anjie <stephano80@gmail.com>
2016-06-27 01:48:33 +0200

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
  var express = require('express');
+ var bodyParser = require('body-parser');
+
  var app = express();
  var PORT = process.env.PORT || 8000;
- var todos = [{
- 	id: 1,
- 	description: 'Maak app af om routes te leren',
- 	completed: false
- }, {
- 	id: 2,
- 	description: 'deploy on Heroku',
- 	completed: false
- },{
- 	id: 3,
- 	description: 'Database koppelen',
- 	completed: false
- }];
+ var todos = [];
+ var todoNextId = 1;
+
+ app.use(bodyParser.json());
 
  app.get('/', function (req, res) {
  	res.send('Root van Todo API');
@@ -32,7 +25,7 @@ app.get('/todos/:id', function (req, res) {
 	todos.forEach(function (todo){
 		if (todoId === todo.id) {
 			matchedTodo = todo;
-		} 
+		}
 	});
 
 	if (matchedTodo) {
@@ -41,6 +34,17 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	}
 })
+
+// POST /todos
+app.post('/todos', function (req, res){
+   var body = req.body;
+
+   body.id = todoNextId++;
+
+   todos.push(body);
+
+   res.json(body);
+});
 
  app.listen(PORT, function () {
  	console.log('Express listening on port ' + PORT + '!');
